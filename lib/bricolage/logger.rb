7115ae88d26d63f4e3a213e51cb0dfecfc98ce2c
Minero Aoki <mineroaoki@gmail.com>
2016-06-23 15:43:47 +0900

    Revert "Support log rotation"

--- lib/bricolage/logger.rb
+++ lib/bricolage/logger.rb
@@ -4,11 +4,7 @@ require 'logger'
 module Bricolage
   class Logger < ::Logger
     def Logger.default
-      shifting
-    end
-
-    def Logger.shifting(age: 0, size: 1048570)
-      logger = new($stderr, age, size)
+      logger = new($stderr)
       logger.level = $stderr.tty? ? Logger::DEBUG : Logger::INFO
       logger.formatter = -> (sev, time, prog, msg) {
         "#{time}: #{sev}: #{msg}\n"
